a119792d071c33c7a7e3eaac4ec02f1a65455d64
zachnewburgh <zach.newburgh@gmail.com>
2016-05-24 14:43:36 -0400

    created shifts_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :phone, :role, :rating, :avatar)
+      params.require(:user).permit(:name, :email, :phone, :role, :rating, :avatar, :gender)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524165457) do
+ActiveRecord::Schema.define(version: 20160524174124) do
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160524165457) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
