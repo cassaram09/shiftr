f565bef25ef189337776dbfebe6217d54cd3bb75
zachnewburgh <zach.newburgh@gmail.com>
2016-05-25 15:41:10 -0400

    working on partial for _admin.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :phone, :role, :rating, :avatar, :gender, :schedule)
+      params.require(:user).permit(:name, :email, :phone, :role, :rating, :avatar, :gender, :t_schedule)
     end
 
 end
