ebe1561996e08aa186256f4a22bb4ba65a00c48d
zachnewburgh <zach.newburgh@gmail.com>
2016-05-24 21:37:15 -0400

    defined schedule#add_shift

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,7 @@
 class Schedule < ActiveRecord::Base
   has_many :scheduled_shifts
+
+  def add_shift(shift_id)
+    scheduled_shift = scheduled_shifts.find_by(scheduled_shift_id: scheduled_shift_id) 
+  end
 end
