c09e8880a938f7ca315caa8e2f9da1f35a3eb155
zachnewburgh <zach.newburgh@gmail.com>
2016-05-26 19:21:39 -0400

    refactored shifts#index, so that shifts are iterative

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :admin
+
+    def admin
+      admin_email_array = ["zach.newburgh@gmail.com"]
+      admin_email_array.include?(current_user.email)
+    end
+
 end
