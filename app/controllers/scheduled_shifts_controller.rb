21dc6ae98584758ca45e79771ded342690e95189
zachnewburgh <zach.newburgh@gmail.com>
2016-05-31 10:16:58 -0400

    added :days helper_method and replaced array of weekdays, accordingly

--- app/controllers/scheduled_shifts_controller.rb
+++ app/controllers/scheduled_shifts_controller.rb
@@ -1,13 +1,12 @@
 class ScheduledShiftsController < ApplicationController
   
   def create
+    @user = current_user
     if !schedule
-      @user = current_user
       @user.schedule = Schedule.create(user_id: @user.id)
       scheduled_shift = @user.schedule.add_shift(params[:shift_id])
       scheduled_shift.save
     else
-      @user = current_user
       scheduled_shift = @user.schedule.add_shift(params[:shift_id])
       scheduled_shift.save
     end
