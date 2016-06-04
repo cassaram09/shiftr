776887883d353b713c010f3cab2001bc4014a2fd
zachnewburgh <zach.newburgh@gmail.com>
2016-06-03 22:21:33 -0400

    created 'tag > post_tag < post' association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
+
+  has_many :post_tags
+  has_many :tags, through: :post_tags
 end
--- db/migrate/20160604021810_create_post_tags.rb
+++ db/migrate/20160604021810_create_post_tags.rb
@@ -1,7 +1,8 @@
 class CreatePostTags < ActiveRecord::Migration
   def change
     create_table :post_tags do |t|
-
+      t.belongs_to :post
+      t.belongs_to :tag
       t.timestamps null: false
     end
   end
