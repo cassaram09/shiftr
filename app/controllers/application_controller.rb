d9e1f0f34a95ee3c2a040a3b9fc54f28a4d143e1
zachnewburgh <zach.newburgh@gmail.com>
2016-06-04 12:05:54 -0400

    added authenticate_user! method before every action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :admin, :days
+  before_action :authenticate_user!
 
   def admin
     admin_email_array = ["zach.newburgh@gmail.com"]
