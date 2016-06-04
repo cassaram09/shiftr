976af66d5cf92e8ef53a0b517c06b9a843e943a3
zachnewburgh <zach.newburgh@gmail.com>
2016-06-03 21:47:44 -0400

    added new and create actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,13 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create!(title: params[:title], body: params[:body])
+    redirect_to posts_path
+  end
 end
