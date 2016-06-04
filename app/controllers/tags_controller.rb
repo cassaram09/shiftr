e55099887d27662441f53ad12eb4e2f6665a9d14
zachnewburgh <zach.newburgh@gmail.com>
2016-06-04 00:35:21 -0400

    added destroy action to tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,4 +3,10 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to posts_path
+  end
 end
