c63a7a38f7a5535ca41a9f5077a5acff0f6f2260
zachnewburgh <zach.newburgh@gmail.com>
2016-06-03 23:58:03 -0400

    added update action to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,12 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
   end
 
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to posts_path
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:body, :post_id, :user_id)
