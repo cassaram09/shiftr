918513190d8627d56a8d00b19cd165d6fbe56d4f
zachnewburgh <zach.newburgh@gmail.com>
2016-06-03 23:27:31 -0400

    created comment_params and comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,15 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to posts_path
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :post_id, :user_id)
+  end
+
 end
