4500d0483677e03dfeb91b1090ce0ed0618fe0c8
zachnewburgh <zach.newburgh@gmail.com>
2016-06-05 11:21:01 -0400

    added show action to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post }
+    end
+  end
+
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
