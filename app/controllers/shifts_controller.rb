b51b2da5869a8f0889cea8af1f65a19d1607b9f3
zachnewburgh <zach.newburgh@gmail.com>
2016-06-06 11:35:36 -0400

    allow json rendering of index

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -3,6 +3,10 @@ class ShiftsController < ApplicationController
 
   def index
     @shifts = Shift.all
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @shifts }
+    end
   end
 
   def new
