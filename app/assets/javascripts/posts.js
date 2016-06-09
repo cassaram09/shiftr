e67b500f27dc23d4ff2f2f2f2c775276872c27be
zachnewburgh <zach.newburgh@gmail.com>
2016-06-09 19:26:07 -0400

    ajax call, logging data to console

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('a#load_comments').on('click', function(e){
+  
+    $.ajax({
+      method: "GET",
+      url: this.href,
+    }).done(function(data) {
+        console.log(data)
+    });
+
+  e.preventDefault();
+  });
+})
