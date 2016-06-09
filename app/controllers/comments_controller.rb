67cda26af8b8587b6249cc2acfed325c695acb10
zachnewburgh <zach.newburgh@gmail.com>
2016-06-09 14:45:52 -0400

    changed render :show to render json: @comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
         format.js   { }
-        format.json { render :show, status: :created, location: @comment }
+        format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
