f26cd1a6b8adba060a29b157bec4eb303b29408a
zachnewburgh <zach.newburgh@gmail.com>
2016-06-08 23:13:12 -0400

    updated create action to include different formatting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,8 +13,16 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-    @comment.save
-    redirect_to posts_path
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
+        format.js   { }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
