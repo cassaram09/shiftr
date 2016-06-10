266924aca1e4f9ad17b79bcd9f9552ced30f5337
zachnewburgh <zach.newburgh@gmail.com>
2016-06-09 22:13:12 -0400

    added new and create actions; added comment_params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,24 +7,19 @@ class CommentsController < ApplicationController
     render json: @comments
   end
 
-  # def new
-  #   @post = Post.find(params[:post_id])
-  #   @comment = Comment.new
-  # end
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
 
-  # def create
-  #   @comment = Comment.new(comment_params)
-  #   respond_to do |format|
-  #     if @comment.save
-  #       format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
-  #       format.js   { }
-  #       format.json { render json: @comment, status: :created, location: @comment }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @comment.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  def create
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      redirect_to @post
+    else
+      render 'posts/show'
+    end
+  end
 
   # def show
   #   @post = Post.find(params[:post_id])
@@ -46,9 +41,9 @@ class CommentsController < ApplicationController
   #   redirect_to posts_path
   # end
 
-  # private
-  # def comment_params
-  #   params.require(:comment).permit(:body, :post_id, :user_id)
-  # end
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :post_id, :user_id)
+  end
 
 end
