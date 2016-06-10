4eb6885b293e3f1423a0bf49cf2b6d1c09ae6c27
zachnewburgh <zach.newburgh@gmail.com>
2016-06-09 22:41:26 -0400

    began ajax post request

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -10,3 +10,26 @@ $(function() {
     e.preventDefault();
   });
 })
+
+$(function() {
+    $('form#new_comment').on('submit', function(e) {
+        url = this.action
+        data = {
+            'authenticity_token': $("input[name='authenticity_token']").val(),
+            'comment': {
+                'body': $('#comment_body').val()
+            }
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: url,
+            data: data,
+            success: function(response){
+                debugger
+            }
+        });
+
+        e.preventDefault();
+    })
+})
