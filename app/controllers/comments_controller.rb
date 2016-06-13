1c337b0317c242c062b63f1bdf5fe419226f5edd
zachnewburgh <zach.newburgh@gmail.com>
2016-06-13 12:15:24 -0400

    commented out show, edit, and update actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,25 +18,25 @@ class CommentsController < ApplicationController
     end
   end
 
-  def show
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @comment }
-    end
-  end
+  # def show
+  #   @post = Post.find(params[:post_id])
+  #   @comment = Comment.find(params[:id])
+  #   respond_to do |format|
+  #     format.html { render :show }
+  #     format.json { render json: @comment }
+  #   end
+  # end
 
-  def edit
-    @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-  end
+  # def edit
+  #   @post = Post.find(params[:post_id])
+  #   @comment = Comment.find(params[:id])
+  # end
 
-  def update
-    @comment = Comment.find(params[:id])
-    @comment.update(comment_params)
-    redirect_to posts_path
-  end
+  # def update
+  #   @comment = Comment.find(params[:id])
+  #   @comment.update(comment_params)
+  #   redirect_to posts_path
+  # end
 
   private
   def comment_params
