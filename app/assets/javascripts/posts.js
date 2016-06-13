18bbe88ad33bd250c67ab46a73212033605d5e16
zachnewburgh <zach.newburgh@gmail.com>
2016-06-13 15:54:13 -0400

    added Comment object, formatter prototype method, and added it to the ajax post

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,12 @@
+function Comment(id, body) {
+  this.id = id;
+  this.body = body;
+}
+
+Comment.prototype.formatter = function() {
+  return this.body.toUpperCase();
+};
+
 $(function() {
   $('#new_comment').on('submit', function(e) {
     e.preventDefault();
@@ -8,9 +17,11 @@ $(function() {
       dataType: 'json',
       data: $(this).serialize(),
       success: function(json) {
+        comment = json.comment
+        response = new Comment(comment.id, comment.body)
         var comment = json.comment;
         var $div = $('div#comments');
-        $div.append("<p>" + comment.body + "<p>");
+        $div.append("<p>" + response.formatter() + "<p>");
         $('#comment_body').val('');
       }
     })
