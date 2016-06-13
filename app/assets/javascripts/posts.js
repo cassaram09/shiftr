adf3ddb3cbaeca2647b5f90dc7c86d2fb72e9150
zachnewburgh <zach.newburgh@gmail.com>
2016-06-13 12:43:23 -0400

    working on json submitting

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,15 +2,26 @@ $(function() {
   $('#new_comment').on('submit', function(e) {
     e.preventDefault();
 
-    $.ajax({
-      type: ($("input[name='_method']").val() || this.method),
-      url: this.action,
-      data: $(this).serialize(),
-      success: function(response) {
-        var $div = $('div#comments')
-        $div.append(response);
-        $('#comment_body').val('');
-      }
+    // AJAXING
+    // $.ajax({
+    //   type: ($("input[name='_method']").val() || this.method),
+    //   url: this.action,
+    //   data: $(this).serialize(),
+    //   success: function(response) {
+    //     var $div = $('div#comments')
+    //     $div.append(response);
+    //     $('#comment_body').val('');
+    //   }
+    // })
+    var action = this.action;
+    var params = $(this).serialize();
+
+    $.post(action, params).success(function(json) {
+      debugger
+      console.log(json)
+    }).error(function(response) {
+      console.log("Oops", response)
     })
+
   })
 });
